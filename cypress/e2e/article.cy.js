7a0839f52636045932576a67af9975960f9274d8
Anna Nikiforova <annanicky90@gmail.com>
2026-01-16 17:15:50 +0400

    cypress: article flow solution

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,13 +1,72 @@
-describe('', () => {
-  before(() => {
+import { faker } from '@faker-js/faker';
 
+// function generateArticle() {
+//   return {
+//     title: faker.lorem.sentence(),
+//     description: faker.lorem.sentences(2),
+//     content: faker.lorem.paragraphs(2),
+//     tags: [
+//       faker.lorem.word(),
+//       faker.lorem.word(),
+//       faker.lorem.word()
+//     ]
+//   };
+// }
+
+describe('Articles flow', () => {
+  let user;
+  let article;
+
+  beforeEach(() => {
+    user = {
+      email: faker.internet.email(),
+      username: (
+        faker.helpers.replaceSymbols('?', {
+          symbols: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+        }) +
+      faker.internet.userName()
+        .replace(/[^A-Za-z0-9_-]/g, '')
+        .replace(/^[^A-Za-z]+/, '')
+      ).toLowerCase(),
+      password: faker.internet.password()
+    };
+    article = {
+      title: faker.lorem.sentence(),
+      description: faker.lorem.sentence(5),
+      body: faker.lorem.paragraphs(2),
+      tags: [
+        faker.lorem.word(),
+        faker.lorem.word(),
+        faker.lorem.word()
+      ]
+    };
+    cy.visit('/');
+    cy.login(user.email, user.username, user.password);
   });
 
-  it('', () => {
+  it('should create an article', () => {
+    cy.visit('/editor');
 
+    cy.get('input[placeholder="Article Title"]').type(article.title);
+    cy.get('input[placeholder="What\'s this article about?"]')
+      .type(article.description);
+    cy.get('textarea[placeholder="Write your article (in markdown)"]')
+      .type(article.body);
+    article.tags.forEach((tag) => {
+      cy.get('input[placeholder="Enter tags"]').type(tag + '{enter}');
+    });
+    cy.contains('button', 'Publish Article').click();
+
+    cy.contains('h1', article.title).should('be.visible');
   });
 
-  it('', () => {
+  it('should delete article', () => {
+    cy.createArticle(article.title, article.description, article.body);
 
+    cy.visit(`/profile/${user.username}`);
+    cy.contains('h1', article.title).click();
+    cy.contains('button', 'Delete Article').click();
+    cy.visit(`/profile/${user.username}`);
+    cy.contains(article.title).should('not.exist');
   });
 });
